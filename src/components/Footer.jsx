25a7132d346f36b9637602a6487c441d4ee64da1
S.Shramko <shramko.web@yahoo.com>
2020-11-29 00:44:53 +0200

    chore(footer): add link to myself
    add since date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const Footer = () => {
+  const sinceDate = "2020-11-28T22:37:09.049Z";
+
   return (
     <footer>
-      <p>&copy; Slick's Slices {new Date().getFullYear()}</p>
+      <p>&copy; Slick's Slices since {new Date(sinceDate).getFullYear()}</p>
+
+      <p>Made with ❤️‍ by - <a href='https://shramko.xyz/' target='_blank' rel="noopener">Serhii Shramko</a></p>
     </footer>
   );
 };
