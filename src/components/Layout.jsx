be7b70fae02b91d9965348b551c4e96cded73018
S.Shramko <shramko.web@yahoo.com>
2020-11-28 22:26:17 +0200

    feat: add basic GlobalStyles optimize svg images

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import "normalize.css";
 
 import Navigation from "./Navigation";
 import Footer from "./Footer";
+import GlobalStyles from "../styles/GlobalStyles";
 
 const Layout = (props) => {
   const { children } = props;
 
   return (
     <div>
+      <GlobalStyles/>
+
       <Navigation/>
 
       {children}
